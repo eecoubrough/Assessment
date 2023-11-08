af3824b4acb7df5fe3dbe4c5e8f1bff37a01ac44
Elise Coubrough <eecoubrough@dundee.ac.uk>
2023-11-08 12:12:39 +0000

    fixed the centering of the map

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 function initMap() {
     // Create a map centered around the UK
     const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 54.0, lng: -2.0 },
+        center: { lat:  54.70897993855097, lng: -3.03060503561271},
         zoom: 5.5
     })
+    
 
 //D3 program that consumes the feed
    d3.json("http://34.38.72.236/Circles/Towns/50", function(data) {
@@ -14,7 +15,6 @@ function initMap() {
 //Plot the towns from the JSON feed onto the map
     d3.json("http://34.38.72.236/Circles/Towns/50")
         .then(data => {
-            //Assuming the JSON contains an array of town objects with lat and lng properties
            data.forEach(town => {
                 const position = { lat: town.lat, lng: town.lng };
 
@@ -22,7 +22,7 @@ function initMap() {
                 new google.maps.Marker({
                     position: position,
                     map: map,
-                   title: town.name // You should adjust this to match your JSON structure
+                   title: town.name
                 });
            });
        })
